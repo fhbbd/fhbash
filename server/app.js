83d5c530daf345fda6d702730a3e75e404ff3f8a
FHB <fhb.ashkona@gmail.com>
2024-06-26 15:17:56 +0600

    my commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 const teacherRouter = require('./route/teacherRoute')
+const { default: mongoose } = require('mongoose')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+
+
+mongoose.connect(MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 app.use('/api/teacher',  teacherRouter)
 
